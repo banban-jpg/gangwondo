86d45a258dc8a92b8a2e688256b3c041435c05d1
banban-jpg <qkswnsdud@gmail.com>
2025-02-20 12:49:22 +0900

    잠시 2,3일차 주석 처리

--- travel-itinerary.js
+++ travel-itinerary.js
@@ -66,31 +66,31 @@ const itineraryData1 = [
   },
 ];
 
-// 1일차, 2일차, 3일차 일정 데이터
-const itineraryData2 = [
-  {
-    title: "아바이마을 오징어순대",
-    location: null,
-    time: "기상 시간에 맞춰서 ~ 06:00",
-    hours: null,
-    entryFee: "확인 필요",
-    specialNotes: "아바이마을 신다신 오징어순대 등 유명한곳 다 맛 똑같음",
-  },
+// // 1일차, 2일차, 3일차 일정 데이터
+// const itineraryData2 = [
+//   {
+//     title: "아바이마을 오징어순대",
+//     location: null,
+//     time: "기상 시간에 맞춰서 ~ 06:00",
+//     hours: null,
+//     entryFee: "확인 필요",
+//     specialNotes: "아바이마을 신다신 오징어순대 등 유명한곳 다 맛 똑같음",
+//   },
   
-];
+// ];
 
-// 1일차, 2일차, 3일차 일정 데이터
-const itineraryData3 = [
-  {
-    title: "기상 후 안반데기로 출발",
-    location: null,
-    time: "00:00 ~ 05:30",
-    hours: null,
-    entryFee: null,
-    specialNotes: "안반데기",
-  },
+// // 1일차, 2일차, 3일차 일정 데이터
+// const itineraryData3 = [
+//   {
+//     title: "기상 후 안반데기로 출발",
+//     location: null,
+//     time: "00:00 ~ 05:30",
+//     hours: null,
+//     entryFee: null,
+//     specialNotes: "안반데기",
+//   },
   
-];
+// ];
 const extraDestinations = [
   {
     name: "경포해변",
